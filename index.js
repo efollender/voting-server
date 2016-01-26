d4d0e5993d6a1bd1de60fa1e30bae8b0395aa411
Emma Follender <follender@brooklynunited.com>
2016-01-26 15:19:40 -0500

    Fixed typo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import makeStore from './src/store';
-import {startServer} from './src/server';
+import startServer from './src/server';
 
 export const store = makeStore();
+
 startServer(store);
 
 store.dispatch({
